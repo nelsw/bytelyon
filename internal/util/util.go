9e36d3246c307c97cea0f07dd57c075902a1af0b
nelsw <connorvanelswyk@gmail.com>
2026-02-20 19:24:52 -0500

    add new utils

--- internal/util/util.go
+++ internal/util/util.go
@@ -3,8 +3,12 @@ package util
 import (
 	"encoding/json"
 	"fmt"
+	"io/fs"
 	"math/rand"
+	"os"
 	"strings"
+
+	"github.com/rs/zerolog/log"
 )
 
 func Ptr[T any](a T) *T { return &a }
@@ -27,4 +31,33 @@ func Domain(s string) string {
 func PrettyPrintln(a any) {
 	b, _ := json.MarshalIndent(a, "", "  ")
 	fmt.Println(string(b))
+}
+
+func Check(err error) {
+	if err != nil {
+		log.Panic().Err(err).Stack().Send()
+	}
+}
+
+func Must[T any](t T, err error) T {
+	Check(err)
+	return t
+}
+
+func RootDir() string {
+	dir := Must(os.Getwd())
+	for !strings.HasSuffix(dir, "bytelyon") {
+		dir = dir[:strings.LastIndex(dir, "/")]
+	}
+
+	return dir
+}
+
+func BinDir(parts ...string) string {
+	dir := RootDir() + "/bin"
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		Check(os.Mkdir(dir, fs.ModePerm))
+	}
+
+	return dir + strings.Join(parts, "/")
 }

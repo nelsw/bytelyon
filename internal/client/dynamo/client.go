5b59b4e010ae66fe088d3a27240bb32d817ab8b6
Connor Van Elswyk <ConnorVanElswyk@gmail.com>
2026-02-28 06:32:01 -0500

    dynamo working

---
+++ internal/client/dynamo/client.go
@@ -0,0 +1,284 @@
+package dynamo
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/expression"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/google/uuid"
+	"github.com/rs/zerolog/log"
+)
+
+var (
+	NotFoundEx    *types.ResourceNotFoundException
+	TableExistsEx *types.TableAlreadyExistsException
+)
+
+// TableExists determines whether a DynamoDB table exists.
+func TableExists(ctx context.Context, dbc *dynamodb.Client, name string) (bool, error) {
+	log.Trace().Str("name", name).Msg("checking if table exists")
+
+	_, err := dbc.DescribeTable(ctx, &dynamodb.DescribeTableInput{
+		TableName: &name,
+	})
+
+	if errors.As(err, &NotFoundEx) {
+		log.Debug().Str("name", name).Msg("table does not exist")
+		return false, nil
+	}
+
+	if err != nil {
+		log.Err(err).Str("name", name).Msg("failed to determine table existence")
+		return false, err
+	}
+
+	log.Debug().Str("name", name).Msg("table exists")
+	return true, nil
+}
+
+// CreateTable creates a DynamoDB table.
+func CreateTable(ctx context.Context, dbc *dynamodb.Client, input *dynamodb.CreateTableInput) error {
+	log.Trace().Str("name", *input.TableName).Msg("creating table")
+
+	_, err := dbc.CreateTable(ctx, input)
+
+	if errors.As(err, &TableExistsEx) {
+		log.Warn().Str("name", *input.TableName).Msg("table already exists")
+		return nil
+	}
+
+	if err != nil {
+		log.Err(err).Str("name", *input.TableName).Msg("failed to create table")
+		return err
+	}
+
+	err = dynamodb.NewTableExistsWaiter(dbc).Wait(ctx, &dynamodb.DescribeTableInput{
+		TableName: input.TableName,
+	}, 5*time.Minute)
+
+	if err != nil {
+		log.Err(err).Str("name", *input.TableName).Msg("failed to wait for table creation")
+		return err
+	}
+
+	log.Debug().Str("name", *input.TableName).Msg("table created")
+	return nil
+}
+
+// DeleteTable deletes the DynamoDB table and all of its data.
+func DeleteTable(ctx context.Context, dbc *dynamodb.Client, name string) error {
+	log.Trace().Str("name", name).Msg("deleting table")
+
+	_, err := dbc.DeleteTable(ctx, &dynamodb.DeleteTableInput{
+		TableName: &name,
+	})
+
+	if errors.As(err, &NotFoundEx) {
+		log.Warn().Str("name", name).Msg("table does not exist")
+		return nil
+	}
+
+	if err != nil {
+		log.Err(err).Str("name", name).Msg("failed to delete table")
+		return err
+	}
+
+	err = dynamodb.NewTableNotExistsWaiter(dbc).Wait(ctx, &dynamodb.DescribeTableInput{
+		TableName: &name,
+	}, 5*time.Minute)
+
+	if err != nil {
+		log.Warn().Err(err).Str("name", name).Msg("failed to wait for table deletion")
+		return nil
+	}
+
+	log.Debug().Str("name", name).Msg("deleted table")
+	return nil
+}
+
+// ListTables lists the DynamoDB table names for the current account.
+func ListTables(ctx context.Context, dbc *dynamodb.Client) ([]string, error) {
+	log.Trace().Msg("listing tables")
+
+	var names []string
+	var output *dynamodb.ListTablesOutput
+	var err error
+
+	tablePaginator := dynamodb.NewListTablesPaginator(dbc, &dynamodb.ListTablesInput{})
+
+	for tablePaginator.HasMorePages() {
+		if output, err = tablePaginator.NextPage(ctx); err != nil {
+			log.Err(err).Msg("failed to list tables")
+			return nil, err
+		}
+		names = append(names, output.TableNames...)
+	}
+
+	log.Debug().Strs("names", names).Msg("listed tables")
+
+	return names, nil
+}
+
+// CreateItem creates a new item, or replaces an old item with a new item.
+func CreateItem(ctx context.Context, dbc *dynamodb.Client, name string, a any) error {
+
+	log.Trace().Str("name", name).Msg("creating item")
+
+	item, err := attributevalue.MarshalMap(a)
+	if err != nil {
+		log.Err(err).Str("name", name).Msg("failed to marshal item")
+		return err
+	}
+
+	_, err = dbc.PutItem(ctx, &dynamodb.PutItemInput{
+		Item:      item,
+		TableName: &name,
+	})
+
+	if err != nil {
+		log.Err(err).Str("name", name).Msg("failed to put item")
+		return err
+	}
+
+	log.Debug().Str("name", name).Msg("item created")
+
+	return nil
+}
+
+// DeleteItem removes an item from a DynamoDB table.
+func DeleteItem(ctx context.Context, dbc *dynamodb.Client, name string, a any) error {
+
+	log.Trace().Str("name", name).Msg("deleting item")
+
+	key, err := attributevalue.MarshalMap(a)
+	if err != nil {
+		log.Err(err).Str("name", name).Msg("failed to marshal key")
+		return err
+	}
+
+	_, err = dbc.DeleteItem(ctx, &dynamodb.DeleteItemInput{
+		TableName: &name,
+		Key:       key,
+	})
+
+	if errors.As(err, &NotFoundEx) {
+		log.Warn().Str("name", name).Msg("item does not exist")
+		return nil
+	}
+
+	if err != nil {
+		log.Err(err).Str("name", name).Msg("failed to delete item")
+		return err
+	}
+
+	log.Debug().Str("name", name).Msg("item deleted")
+
+	return nil
+}
+
+// GetItem retrieves an item from the DynamoDB table.
+func GetItem[T any](ctx context.Context, dbc *dynamodb.Client, name string, a any) (t T, err error) {
+
+	log.Trace().Str("name", name).Msg("getting item")
+
+	var key map[string]types.AttributeValue
+	if key, err = attributevalue.MarshalMap(a); err != nil {
+		log.Err(err).Str("name", name).Msg("failed to marshal key")
+		return
+	}
+
+	var res *dynamodb.GetItemOutput
+	res, err = dbc.GetItem(ctx, &dynamodb.GetItemInput{
+		TableName: &name,
+		Key:       key,
+	})
+
+	if err != nil {
+		log.Err(err).Str("name", name).Msg("failed to get item")
+		return
+	}
+
+	if res.Item == nil {
+		log.Warn().Str("name", name).Msg("item not found")
+		err = NotFoundEx
+		return
+	}
+
+	if err = attributevalue.UnmarshalMap(res.Item, &t); err != nil {
+		log.Err(err).Str("name", name).Msg("failed to unmarshal item")
+		return
+	}
+
+	log.Debug().Str("name", name).Any("item", t).Msg("got item")
+
+	return
+}
+
+// QueryByID gets all items in the DynamoDB table by the hash key.
+func QueryByID[T any](ctx context.Context, dbc *dynamodb.Client, name, key string, val uuid.UUID) ([]T, error) {
+
+	l := log.With().
+		Str("name", name).
+		Str("key", key).
+		Stringer("val", val).
+		Logger()
+
+	l.Trace().Msg("querying")
+
+	expr, err := expression.
+		NewBuilder().
+		WithKeyCondition(expression.Key(key).Equal(expression.Value(val))).
+		Build()
+
+	if err != nil {
+		l.Err(err).Msg("failed to build expression")
+		return nil, err
+	}
+
+	queryPaginator := dynamodb.NewQueryPaginator(dbc, &dynamodb.QueryInput{
+		TableName:                 &name,
+		ExpressionAttributeNames:  expr.Names(),
+		ExpressionAttributeValues: expr.Values(),
+		KeyConditionExpression:    expr.KeyCondition(),
+	})
+
+	var response *dynamodb.QueryOutput
+	var arr []T
+	for queryPaginator.HasMorePages() {
+
+		if response, err = queryPaginator.NextPage(ctx); err != nil {
+			l.Err(err).Msg("failed to query")
+			return nil, err
+		}
+
+		var tt []T
+		if err = attributevalue.UnmarshalListOfMaps(response.Items, &tt); err != nil {
+			l.Err(err).Msg("failed to unmarshal items")
+			return nil, err
+		}
+		arr = append(arr, tt...)
+	}
+
+	l.Debug().Int("size", len(arr)).Msg("queried")
+
+	return arr, err
+}
+
+// New returns a new DynamoDB client with the given Region, AccessKeyID, and SecretAccessKey.
+func New(reg, aki, sac string) *dynamodb.Client {
+	return dynamodb.NewFromConfig(aws.Config{
+		Credentials: credentials.StaticCredentialsProvider{
+			Value: aws.Credentials{
+				AccessKeyID:     aki,
+				SecretAccessKey: sac,
+			},
+		},
+		Region: reg,
+	})
+}
---
+++ test/client_dynamo_test.go
@@ -0,0 +1,172 @@
+package test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/google/uuid"
+	"github.com/nelsw/bytelyon/internal/client/dynamo"
+	"github.com/nelsw/bytelyon/internal/config"
+	"github.com/nelsw/bytelyon/internal/util"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Client_Dynamo_Table_Functions(t *testing.T) {
+
+	var err error
+	var ok bool
+	var names []string
+
+	ctx := context.Background()
+	dbc := dynamo.New(
+		config.Get[string]("AWS_REGION"),
+		config.Get[string]("AWS_ACCESS_KEY_ID"),
+		config.Get[string]("AWS_SECRET_ACCESS_KEY"),
+	)
+	name := "ByteLyon_Test_Table"
+
+	ok, err = dynamo.TableExists(ctx, dbc, name)
+	assert.NoError(t, err)
+	assert.False(t, ok)
+
+	err = dynamo.CreateTable(ctx, dbc, &dynamodb.CreateTableInput{
+		TableName:   &name,
+		BillingMode: types.BillingModeProvisioned,
+		ProvisionedThroughput: &types.ProvisionedThroughput{
+			ReadCapacityUnits:  util.Ptr(int64(10)),
+			WriteCapacityUnits: util.Ptr(int64(10)),
+		},
+		KeySchema: []types.KeySchemaElement{{
+			AttributeName: util.Ptr("ID"),
+			KeyType:       types.KeyTypeHash,
+		}},
+		AttributeDefinitions: []types.AttributeDefinition{{
+			AttributeName: util.Ptr("ID"),
+			AttributeType: types.ScalarAttributeTypeB,
+		}},
+	})
+	assert.NoError(t, err)
+
+	ok, err = dynamo.TableExists(ctx, dbc, name)
+	assert.NoError(t, err)
+	assert.True(t, ok)
+
+	names, err = dynamo.ListTables(ctx, dbc)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, names)
+
+	err = dynamo.DeleteTable(ctx, dbc, name)
+	assert.NoError(t, err)
+
+	ok, err = dynamo.TableExists(ctx, dbc, name)
+	assert.NoError(t, err)
+	assert.False(t, ok)
+}
+
+func Test_Client_Dynamo_Item_Functions(t *testing.T) {
+
+	var err error
+	ctx := context.Background()
+	dbc := dynamo.New(
+		config.Get[string]("AWS_REGION"),
+		config.Get[string]("AWS_ACCESS_KEY_ID"),
+		config.Get[string]("AWS_SECRET_ACCESS_KEY"),
+	)
+	name := "ByteLyon_Test_User"
+
+	type User struct {
+		ID uuid.UUID `json:"id" dynamodbav:"ID,binary"`
+	}
+
+	var exp = User{util.Must(uuid.NewV7())}
+
+	err = dynamo.CreateItem(ctx, dbc, name, exp)
+	assert.NoError(t, err)
+
+	var act = User{exp.ID}
+	act, err = dynamo.GetItem[User](ctx, dbc, name, exp)
+	assert.NoError(t, err)
+	assert.Equal(t, exp.ID, act.ID)
+
+	err = dynamo.DeleteItem(ctx, dbc, name, exp)
+	assert.NoError(t, err)
+
+	act, err = dynamo.GetItem[User](ctx, dbc, name, exp)
+	assert.ErrorAs(t, err, &dynamo.NotFoundEx)
+}
+
+func Test_Client_Dynamo_Query(t *testing.T) {
+
+	var err error
+	ctx := context.Background()
+	dbc := dynamo.New(
+		config.Get[string]("AWS_REGION"),
+		config.Get[string]("AWS_ACCESS_KEY_ID"),
+		config.Get[string]("AWS_SECRET_ACCESS_KEY"),
+	)
+	name := "ByteLyon_Test_Bot_News"
+
+	err = dynamo.DeleteTable(ctx, dbc, name)
+	assert.NoError(t, err)
+
+	err = dynamo.CreateTable(ctx, dbc, &dynamodb.CreateTableInput{
+		TableName:   &name,
+		BillingMode: types.BillingModeProvisioned,
+		ProvisionedThroughput: &types.ProvisionedThroughput{
+			ReadCapacityUnits:  util.Ptr(int64(10)),
+			WriteCapacityUnits: util.Ptr(int64(10)),
+		},
+		KeySchema: []types.KeySchemaElement{{
+			AttributeName: util.Ptr("UserID"),
+			KeyType:       types.KeyTypeHash,
+		}, {
+			AttributeName: util.Ptr("ID"),
+			KeyType:       types.KeyTypeRange,
+		}},
+		AttributeDefinitions: []types.AttributeDefinition{{
+			AttributeName: util.Ptr("UserID"),
+			AttributeType: types.ScalarAttributeTypeB,
+		}, {
+			AttributeName: util.Ptr("ID"),
+			AttributeType: types.ScalarAttributeTypeB,
+		}},
+	})
+	assert.NoError(t, err)
+
+	type Bot struct {
+		ID        uuid.UUID     `json:"id" dynamodbav:"ID,binary"`
+		UserID    uuid.UUID     `json:"userId" dynamodbav:"UserID,binary"`
+		Frequency time.Duration `json:"frequency" dynamodbav:"Frequency,number"`
+		BlackList []string      `json:"blackList" dynamodbav:"BlackList,stringset"`
+		Headless  bool          `json:"headless" dynamodbav:"Headless,boolean"`
+	}
+
+	ids := []uuid.UUID{
+		util.Must(uuid.NewV7()),
+		util.Must(uuid.NewV7()),
+	}
+
+	for j := 0; j < 2; j++ {
+		for i := 0; i < 5; i++ {
+			err = dynamo.CreateItem(ctx, dbc, name, Bot{
+				UserID:    ids[j],
+				ID:        util.Must(uuid.NewV7()),
+				Frequency: time.Hour * time.Duration(fake.Uint8()),
+				BlackList: []string{
+					fake.DomainName(),
+				},
+				Headless: fake.Bool(),
+			})
+			assert.NoError(t, err)
+		}
+	}
+
+	var arr []Bot
+	arr, err = dynamo.QueryByID[Bot](ctx, dbc, name, "UserID", ids[0])
+	assert.NoError(t, err)
+	assert.Len(t, arr, 5)
+	util.PrettyPrintln(arr)
+}

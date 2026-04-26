5d42eccdd97d2ce586e74929986af495737871e4
Connor Van Elswyk <ConnorVanElswyk@gmail.com>
2026-04-26 08:56:43 -0400

    add url and keywords options to spinarticle func

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -1,18 +1,45 @@
 package service
 
 import (
+	"strings"
+
 	"github.com/nelsw/bytelyon/pkg/client"
 	"github.com/nelsw/bytelyon/pkg/model"
+	"github.com/nelsw/bytelyon/pkg/service/images"
 	"github.com/rs/zerolog/log"
 )
 
-func SpinArticle(a *model.Article) (link string, err error) {
-	if a.Body, err = client.Prompt(a.Prompt, "Write a blog post summarizing this article: "+a.Body); err != nil {
-		log.Warn().Err(err).Msg("failed to spin article html")
-	} else if link, err = client.CreateArticle(a); err != nil {
-		log.Error().Err(err).Msg("Failed to create article on Shopify after spinning it")
-	} else {
-		log.Info().Msg("published article")
+func SpinArticle(a *model.Article) (string, error) {
+
+	if img, ok := a.Image["url"]; ok {
+		if url, err := images.ToPublicURL(img); err != nil {
+			log.Warn().Err(err).Msgf("Failed to convert url to public url")
+			a.Image = nil
+		} else {
+			a.Image["url"] = url
+		}
 	}
-	return
+
+	message := "Write a blog post summarizing this article: " + a.Body
+	if len(a.Keywords) > 0 {
+		message += `\n\ninclude the following keywords: ` + strings.Join(a.Keywords, ",") + `.`
+	}
+	if a.URL != "" {
+		message += `\n\ninclude a link to the original article: ` + a.URL
+	}
+
+	if txt, err := client.Prompt(a.Prompt, message, true); err != nil {
+		log.Err(err).Msg("failed to spin article body")
+	} else {
+		a.Body = txt
+	}
+
+	link, err := client.CreateArticle(a)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to create article on Shopify after spinning it")
+		return "", err
+	}
+
+	log.Info().Msg("published article")
+	return link, nil
 }

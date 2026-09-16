2fa0e65987e4dc7389d7371a434961ca93ab68df
Connor Van Elswyk <connorvanelswyk@gmail.com>
2026-09-16 19:59:43 -0400

    center center (#19)

--- resources/js/layouts/auth/AuthSimpleLayout.vue
+++ resources/js/layouts/auth/AuthSimpleLayout.vue
@@ -11,9 +11,9 @@ defineProps<{
 
 <template>
     <div
-        class="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10"
+        class="flex min-h-svh flex-col items-center justify-center gap-6 overflow-y-auto bg-background p-6 md:p-10"
     >
-        <div class="w-full max-w-sm">
+        <div class="mx-auto w-full max-w-sm">
             <div class="flex flex-col gap-8">
                 <div class="flex flex-col items-center gap-4">
                     <Link
